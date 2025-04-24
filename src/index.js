da10863c048de91f1960d6caed00d8860f42c240
Rustam <189756814+rustleium@users.noreply.github.com>
2025-04-24 12:50:58 +0530

    feat(index.js): render home page on initial load

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import "./style.css";
+import { renderHome } from "./home.js";
+
+window.addEventListener('DOMContentLoaded', () => {
+  const contentDiv = document.getElementById("content");
+  const homeContent = renderHome();
+  contentDiv.appendChild(homeContent);
+});
+
 console.log("it works!");
