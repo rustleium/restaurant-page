ea8924349a1ebe9beb64b0584afb790c49f4b40a
Rustam <189756814+rustleium@users.noreply.github.com>
2025-04-24 13:35:08 +0530

    Fix: correctly import and use image path
    - Updated the `src` attribute of the Butterbeer image
    to use the dynamically imported path provided by Webpack.

--- src/home.js
+++ src/home.js
@@ -1,11 +1,13 @@
+import butterbeer from "./assets/butter-beer.jpg";
+
 function renderHome () {
   const homeContent = document.createElement("div");
   homeContent.innerHTML = `
     <h1 id="headline">Welcome to Rusty Corner</h1>
     <h3>Contrary to popular belief... we don't serve food.</h3>
     <h3>Only Butterbeer.</h3>
-    <img id="image-home" src="/assets/butter-beer.jpg" alt="">
-  `
+    <img id="image-home" src="${butterbeer}" alt="Butterbeer">
+  `;
   return homeContent;
 }
 
