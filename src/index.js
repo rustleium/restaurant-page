7707306fb64ea8b2610e4ad66c2467789c17d916
Rustam <189756814+rustleium@users.noreply.github.com>
2025-04-25 09:04:03 +0530

    feat: render contact page on button click

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import "./style.css";
 import { renderHome } from "./home.js";
+import { renderContact } from "./contact.js";
 
 window.addEventListener('DOMContentLoaded', () => {
   const contentDiv = document.getElementById("content");
   const homeContent = renderHome();
   contentDiv.appendChild(homeContent);
 });
+
+const contactBtn = document.getElementById('contact-btn');
+
+contactBtn.addEventListener('click', () => {
+  const contentDiv = document.getElementById("content");
+  contentDiv.innerHTML = "";
+  const contactContent = renderContact();
+  contentDiv.appendChild(contactContent);
+})
 
 console.log("it works!");
