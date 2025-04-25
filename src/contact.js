81b55ac610f6d5ae418eb56546868a2f4c76f473
Rustam <189756814+rustleium@users.noreply.github.com>
2025-04-25 08:33:39 +0530

    feat(contact): add content rendering function

---
+++ src/contact.js
@@ -0,0 +1,15 @@
+function renderContact () {
+  const contactContent = document.createElement('div');
+  contactContent.innerHTML = `
+    <h1>Contact Us</h1>
+    <p>We prefer owls... but you may use one of these terribly mundane methods:</p>
+    <ul>
+      <li>📧 Email: rustymagic@butterbrew.fake</li>
+      <li>📍 Location: Nowhere near you think.</li>
+      <li>📞 Phone: No. Just no.</li>
+    </ul>
+    <img class="image" id="image-contact" src="" alt="mailbox">
+
+  `
+  return contactContent;
+}
